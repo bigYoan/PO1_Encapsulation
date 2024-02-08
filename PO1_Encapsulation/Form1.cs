84c972537d2ed7b5169df193b8f8fcdfb53b3aa2
bigYoan <2301422@cegepba.qc.ca>
2024-02-08 11:44:10 -0500

    Premieres lignes programmees

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -2,24 +2,50 @@ namespace PO1_Encapsulation
 {
     public partial class Form1 : Form
     {
+        Personnage joueur = new Personnage();
+        Personnage dragon = new Personnage();
+
         public Form1()
         {
             InitializeComponent();
+            joueur.PointsDeVie = 10;
+            joueur.PointsDePuissance = 1;
+
+            dragon.PointsDeVie = 30;
+            dragon.PointsDePuissance = 3;
+
+
+
+            //Personnage bowser = new Personnage();
+            //bowser.PointsDePuissance = -100;
+            //if (bowser.EstEnVie) MessageBox.Show("Toujours vivant");
         }
 
+        private void ActualiserLesLabels()
+        {
+            lbl_pointsVieDragon.Text = "Vie du joueur : " + dragon.PointsDeVie.ToString();
+            // etc pour les autres stats
+
+        }
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
-
+            dragon.PointsDeVie -= joueur.PointsDePuissance;
+            ActualiserLesLabels();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
-
+            ActualiserLesLabels();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            ActualiserLesLabels();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ActualiserLesLabels();
         }
     }
 }
---
+++ PO1_Encapsulation/Personnage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO1_Encapsulation
+{
+    public class Personnage
+    {
+        string nom;
+        int pointsDeVie;
+        int pointsDePuissance;
+
+        // bouton droit action rapide et factorisation , encapsuler les champs et propriétés
+        public string Nom { get => nom; set => nom = value; }
+        public int PointsDeVie { get => pointsDeVie;
+            set
+            {
+                if (value < 0)
+                {
+                    pointsDeVie = 0;
+                }
+                else
+                {
+                    pointsDeVie = value;
+                }
+            }
+                }
+        public int PointsDePuissance { get => pointsDePuissance; set => pointsDePuissance = value; }
+
+        public bool EstEnVie
+        {
+            get
+            {
+                if (pointsDeVie > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
